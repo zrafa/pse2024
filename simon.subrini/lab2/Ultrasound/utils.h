d413aa37ccbdcc6e692392d2bb75acd42bd813c1
SimonSubrini <simonsubrini@gmail.com>
2024-04-28 23:30:09 -0300

    Agrego resolución de los ejercicios 1 y2 del lab2

---
+++ simon.subrini/lab2/Ultrasound/utils.h
@@ -0,0 +1,75 @@
+
+/* utils.h - funciones de soporte al programa principal */
+
+
+
+#ifndef _UTILS_H
+#define _UTILS_H
+
+/** PIN B */
+#define PINB0 0
+#define PINB1 1
+#define PINB2 2
+#define PINB3 3
+#define PINB4 4
+#define PINB5 5
+#define PINB6 6
+#define PINB7 7
+
+/** DDR B */
+#define DDB0 0
+#define DDB1 1
+#define DDB2 2
+#define DDB3 3
+#define DDB4 4
+#define DDB5 5
+#define DDB6 6
+#define DDB7 7
+
+/** Puerto B */
+#define PORTB0 0
+#define PORTB1 1
+#define PORTB2 2
+#define PORTB3 3
+#define PORTB4 4
+#define PORTB5 5
+#define PORTB6 6
+#define PORTB7 7
+
+/** PIN C */
+#define PINC0 0
+#define PINC1 1
+#define PINC2 2
+#define PINC3 3
+#define PINC4 4
+#define PINC5 5
+#define PINC6 6
+
+/** DDR C */
+#define DDC0 0
+#define DDC1 1
+#define DDC2 2
+#define DDC3 3
+#define DDC4 4
+#define DDC5 5
+#define DDC6 6
+
+/** PORT C */
+#define PORTC0 0
+#define PORTC1 1
+#define PORTC2 2
+#define PORTC3 3
+#define PORTC4 4
+#define PORTC5 5
+#define PORTC6 6
+
+
+
+void ultrasound_init(const unsigned char trig_pin, const unsigned char echo_pin);
+void led_on(unsigned char i);
+void led_off(unsigned char i);
+unsigned char read_pin(unsigned char i);
+unsigned char read_registers(unsigned char i);
+
+
+#endif	/* _UTILS_H */
