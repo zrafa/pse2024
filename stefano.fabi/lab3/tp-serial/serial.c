1c83ace8c7b25782dacb57ad2ea2cbe7703548fb
Stefano <stefanofabi96@gmail.com>
2024-05-16 17:54:31 -0300

    Corregir serial
    Indicaba mal los registros. Mejore la legibilidad del codigo

--- stefano.fabi/lab3/tp-serial/serial.c
+++ stefano.fabi/lab3/tp-serial/serial.c
@@ -16,6 +16,41 @@
    del periferico de hardware del USART del atmega328, segun sugerido
    en el apunte */
 
+
+/* UCSR0A - USART Control and Status Register A bits */
+#define UCSR0A_MPCM0   1 << 0
+#define UCSR0A_U2X0    1 << 1
+#define UCSR0A_UPE0    1 << 2
+#define UCSR0A_DOR0    1 << 3
+#define UCSR0A_FE0     1 << 4
+#define UCSR0A_UDRE0   1 << 5
+#define UCSR0A_TXC0    1 << 6
+#define UCSR0A_RXC0    1 << 7
+
+/* UCSR0B - USART Control and Status Register B bits */
+#define UCSR0B_TXB80   1 << 0
+#define UCSR0B_RXB80   1 << 1
+#define UCSR0B_UCSZ02  1 << 2
+#define UCSR0B_TXEN0   1 << 3
+#define UCSR0B_RXEN0   1 << 4
+#define UCSR0B_UDRIE0  1 << 5
+#define UCSR0B_TXCIE0  1 << 6
+#define UCSR0B_RXCIE0  1 << 7
+
+/* UCSR0C - USART Control and Status Register C bits */
+#define UCSR0C_UCPOL0  1 << 0
+#define UCSR0C_UCSZ00  1 << 1
+#define UCSR0C_UCSZ01  1 << 2
+#define UCSR0C_USBS0   1 << 3
+#define UCSR0C_UPM00   1 << 4
+#define UCSR0C_UPM01   1 << 5
+#define UCSR0C_UMSEL00 1 << 6
+#define UCSR0C_UMSEL01 1 << 7
+
+#ifndef F_CPU
+#define F_CPU 4000000UL
+#endif
+
 typedef struct
 {
  uint8_t status_control_a;      /* ucsr0a USART Control and Status A */
@@ -31,16 +66,25 @@ typedef struct
 /* puntero a la estructura de los registros del periferico */
 uart_t *puerto_serial = (uart_t *) (0xc0);
 
-#define USART_BAUDRATE 9600
-//#define BAUD_PRESCALE (((F_CPU/(USART_BAUDRATE*16UL)))-1)   // UBRR (Baud Rate Register) 
-#define BAUD_PRESCALE 0b00011001        
+#define USART_BAUDRATE              9600
+#define BAUD_PRESCALE               (((F_CPU/(USART_BAUDRATE*16UL)))-1)   // UBRR (Baud Rate Register) 
+#define DISABLE_PARITY_MODE         ((UCSR0C_UPM01 & ~UCSR0C_UPM01) | (UCSR0C_UPM00 & ~UCSR0C_UPM00))
+#define ONE_STOP_BIT                (UCSR0C_USBS0 & ~UCSR0C_USBS0)
+#define CHAR_SIZE_8_BITS            (UCSR0C_UCSZ01 | UCSR0C_UCSZ00)
+#define ASYNCHRONOUS_MODE           ((UCSR0C_UMSEL01 & ~UCSR0C_UMSEL01) | (UCSR0C_UMSEL00 & ~UCSR0C_UMSEL00))
 
-#define INIT_C 0x06     // 0b00000110
-#define INIT_B 0x18     // 0b00011000
+// configuracion del usart
+#define USART_MODE      ASYNCHRONOUS_MODE           /* Habilitar la recepción */
+#define RX_ENABLE       UCSR0B_RXEN0                /* Habilitar la recepción */
+#define TX_ENABLE       UCSR0B_TXEN0                /* Habilitar la transmisión */
+#define PARITY_MODE     DISABLE_PARITY_MODE         /* No usar bit de paridad */
+#define STOP_BITS       ONE_STOP_BIT                /* Usar 1 bit de stop */
+#define CHAR_SIZE       CHAR_SIZE_8_BITS            /* Usar 8 bits de dato de E/S */
 
-#define TXEN0   0b01000000
-#define RXCn    0b10000000
-#define UDREn   0b00010000
+// estados
+#define READY_TO_READ   UCSR0A_RXC0                 /* Dato listo para leer */
+#define READY_TO_WRITE  UCSR0A_UDRE0                /* Búfer listo para escribir */
+
 
 // PAGINA 149 ATMEGA328 -- RUTINA INICIALIZACION
 // PAGINA 159 ATMEGA328 -- REGISTROS DEL HARDWARE
@@ -55,26 +99,25 @@ void serial_init() {
 	/* Configurar un frame de 8bits, con un bit de paridad y bit de stop */
 	/* Activar la recepcion y transmicion */
 
-
     /* Escribir una rutina de inicializacion */
     /* El manual del atmega328p tiene un ejemplo. Adecuarla a C y
     la estructura de datos */
     /* Configurar los registros High y Low con BAUD_PRESCALE */
-    puerto_serial->baud_rate_l = (unsigned char) (BAUD_PRESCALE);
-    puerto_serial->baud_rate_h = (unsigned char) (BAUD_PRESCALE >> 8);
-
-    /* Configurar un frame de 8bits, con un bit de paridad y bit de stop */
-    puerto_serial->status_control_c = (unsigned char) (INIT_C);
-
-    /* Activar la recepcion y transmicion */
-    puerto_serial->status_control_b = (unsigned char) (INIT_B);
-
 
     /* Configuracion: 9600bps, 8bits data, 1bit stop, sin bit de paridad
     8 bits de datos: b2:UCSZn2 = 0 en UCSRnB, b2:UCSZn1 = 1 y b1:UCSZn0 = 1 en UCSRnC
     1 bit de stop: b3:USBSn = 0 en UCSRnC
     sin bit de paridad: b5:UPMn1 = 0 y b4:UMPn0 = 0 en UCSRnC 
     */
+
+    puerto_serial->baud_rate_l = (unsigned char) (BAUD_PRESCALE);
+    puerto_serial->baud_rate_h = (unsigned char) (BAUD_PRESCALE >> 8);
+
+    /* Configurar un frame de 8bits, con un bit de paridad y bit de stop */
+    puerto_serial->status_control_c = (unsigned char) (USART_MODE | PARITY_MODE | STOP_BITS | CHAR_SIZE);
+
+    /* Activar la recepcion y transmicion */
+    puerto_serial->status_control_b = (unsigned char) (RX_ENABLE | TX_ENABLE | (UCSR0B_UCSZ02 & ~UCSR0B_UCSZ02));
 }
 
 
@@ -97,7 +140,7 @@ void serial_put_char(char c)
     /* Wait for empty transmit buffer */
     // el dispositivo esta listo para transmitir y
     // el buffer esta vacio
-    while (! (puerto_serial->status_control_a & UDREn));
+    while (! (puerto_serial->status_control_a & READY_TO_WRITE));
 
     /* Put data into buffer, sends the data */
     puerto_serial->data_es = c;
@@ -116,7 +159,7 @@ char serial_get_char(void)
     // return /* DEBE devolver el valor que se encuentra en el registro de datos de E/S */
 
     /* Wait for data to be received */
-    while (! (puerto_serial->status_control_a & RXCn));
+    while (! (puerto_serial->status_control_a & READY_TO_READ));
 
     /* Get and return received data from buffer */
     return puerto_serial->data_es;
