2ea23da5befe464f76095e783915827819ce2b52
Stefano <stefanofabi96@gmail.com>
2024-05-23 09:53:45 -0300

    Corregir buffer de la shell

--- stefano.fabi/lab3/ej5/capturar_teclas.sh
+++ stefano.fabi/lab3/ej5/capturar_teclas.sh
@@ -1,4 +1,4 @@
-stty -F /dev/ttyUSB0 speed 9600 # o 2400 la que necesiten
-cat /dev/ttyUSB0 | while read -n 1 tecla; do
+stty -F /dev/ttyUSB0 speed 9600  # o 2400 la que necesiten
+stdbuf -i0 -o0 cat /dev/ttyUSB0 | while read -n 1 tecla; do
 xdotool key $tecla
 done
