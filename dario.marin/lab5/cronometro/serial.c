5edb298eb69b090932bba804080a34cda4995cf2
Dario A. Marin Roth <dariomarinroth@gmail.com>
2024-06-29 14:07:40 -0300

    ref(lab5): aplicando normas estilo (las mías)

--- dario.marin/lab5/cronometro/serial.c
+++ dario.marin/lab5/cronometro/serial.c
@@ -10,7 +10,7 @@
 #include <stdint.h>
 
 #define USART_BAUDRATE 9600
-#define BAUD_PRESCALE  (((F_OSC/(USART_BAUDRATE*16UL)))-1)
+#define BAUD_PRESCALE (((F_OSC / (USART_BAUDRATE * 16UL))) - 1)
 
 /* UCSR0A - USART Control and Status Register A bits */
 #define UCSR0A_MPCM0 0
@@ -42,11 +42,11 @@
 #define UCSR0C_UMSEL00 6
 #define UCSR0C_UMSEL01 7
 
-#define RX_E (1 << UCSR0B_RXEN0)		   /* Habilitar la recepción */
-#define TX_E (1 << UCSR0B_TXEN0)		   /* Habilitar la transmisión */
-#define PARITY_MODE (0 << UCSR0C_UPM00)	   /* No usar bit de paridad */
-#define STOP_BITS (0 << UCSR0C_USBS0)	   /* Usar 1 bit de stop */
-#define CHAR_SIZE (3 << UCSR0C_UCSZ00)	   /* Usar 8 bits de dato de E/S */
+#define RX_E (1 << UCSR0B_RXEN0)           /* Habilitar la recepción */
+#define TX_E (1 << UCSR0B_TXEN0)           /* Habilitar la transmisión */
+#define PARITY_MODE (0 << UCSR0C_UPM00)    /* No usar bit de paridad */
+#define STOP_BITS (0 << UCSR0C_USBS0)      /* Usar 1 bit de stop */
+#define CHAR_SIZE (3 << UCSR0C_UCSZ00)     /* Usar 8 bits de dato de E/S */
 #define READY_TO_READ (1 << UCSR0A_RXC0)   /* Dato listo para leer */
 #define READY_TO_WRITE (1 << UCSR0A_UDRE0) /* Búfer listo para escribir */
 
@@ -60,133 +60,116 @@
 
 /* Estructura de datos overlay del USART del atmega328 */
 typedef struct {
-	uint8_t status_control_a; /* ucsr0a USART Control and Status A */
-	uint8_t status_control_b; /* ucsr0b USART Control and Status B */
-	uint8_t status_control_c; /* ucsr0c USART Control and Status C */
-	uint8_t _reserved;        /* reserved */
-	uint8_t baud_rate_l;      /* ubrr0l baud rate low */;
-	uint8_t baud_rate_h;      /* ubrr0h baud rate high */
-	uint8_t data_io;          /* udr0 I/O data */
+  uint8_t status_control_a; /* ucsr0a USART Control and Status A */
+  uint8_t status_control_b; /* ucsr0b USART Control and Status B */
+  uint8_t status_control_c; /* ucsr0c USART Control and Status C */
+  uint8_t _reserved;        /* reserved */
+  uint8_t baud_rate_l;      /* ubrr0l baud rate low */
+  ;
+  uint8_t baud_rate_h; /* ubrr0h baud rate high */
+  uint8_t data_io;     /* udr0 I/O data */
 } volatile uart_t;
 
 /* Puntero a la estructura de los registros del periférico */
-uart_t *serial_port = (uart_t*)(0xc0);
+uart_t *serial_port = (uart_t *)(0xc0);
 
-void serial_init(void)
-{
-	/* Configurar los registros High y Low con BAUD_PRESCALE */
-	serial_port->baud_rate_h = (unsigned char)(BAUD_PRESCALE >> 8);
-	serial_port->baud_rate_l = (unsigned char)(BAUD_PRESCALE);
+void serial_init(void) {
+  /* Configurar los registros High y Low con BAUD_PRESCALE */
+  serial_port->baud_rate_h = (unsigned char)(BAUD_PRESCALE >> 8);
+  serial_port->baud_rate_l = (unsigned char)(BAUD_PRESCALE);
 
-	/* Configurar un frame de 8bits, con un bit de paridad y bit de stop */
-	serial_port->status_control_c = CHAR_SIZE | STOP_BITS | PARITY_MODE;
+  /* Configurar un frame de 8bits, con un bit de paridad y bit de stop */
+  serial_port->status_control_c = CHAR_SIZE | STOP_BITS | PARITY_MODE;
 
-	/* Activar la recepcion y transmicion */
-	serial_port->status_control_b = RX_E | TX_E;
+  /* Activar la recepcion y transmicion */
+  serial_port->status_control_b = RX_E | TX_E;
 }
 
-void serial_put_char(char c)
-{
-	while (!(serial_port->status_control_a & READY_TO_WRITE))
-		;
-	serial_port->data_io = c;
+void serial_put_char(char c) {
+  while (!(serial_port->status_control_a & READY_TO_WRITE));
+  serial_port->data_io = c;
 }
 
-char serial_get_char(void)
-{
-	while (!((serial_port->status_control_a) & (READY_TO_READ)))
-		;
-	return (serial_port->data_io);
+char serial_get_char(void) {
+  while (!((serial_port->status_control_a) & (READY_TO_READ)));
+  return (serial_port->data_io);
 }
 
-void serial_put_str(char *str)
-{
-	while (*str != '\0') {
-		serial_put_char(*str);
-		str++;
-	}
+void serial_put_str(char *str) {
+  while (*str != '\0') {
+    serial_put_char(*str);
+    str++;
+  }
 }
 
-char serial_get_str(char * buffer, int max_string_length)
-{
-	int i = 0;
-	char c;
+char serial_get_str(char *buffer, int max_string_length) {
+  int i = 0;
+  char c;
 
-	/*
-	Cuidado, el siguiente es un mensaje valido:
-	'hola mundo \n \r que tal?'
-	*/
-	do {
-		c = serial_get_char();
-		buffer[i] = c;
-		i++;
-	} while (c != '\0' && i < (max_string_length - 1));
+  /*
+  Cuidado, el siguiente es un mensaje valido:
+  'hola mundo \n \r que tal?'
+  */
+  do {
+    c = serial_get_char();
+    buffer[i] = c;
+    i++;
+  } while (c != '\0' && i < (max_string_length - 1));
 
-	/*Se añade el carácter nulo al final del string para marcar su terminación*/
-	buffer[i] = '\0'; 
-	return buffer;
+  /*Se añade el carácter nulo al final del string para marcar su terminación*/
+  buffer[i] = '\0';
+  return buffer;
 }
 
-void serial_put_long_int (long int value, int num_digits)
-{
-	char buffer[MAX_LONG_DIGITS] = {0};
-	int i = 0;
+void serial_put_long_int(long int value, int num_digits) {
+  char buffer[MAX_LONG_DIGITS] = {0};
+  int i = 0;
 
-	if (value == 0) {
-		return serial_put_char('0');
-	} else if (value < 0) {
-		serial_put_char('-');
-		value = -value; // Usar valor absoluto
-	}
+  if (value == 0) {
+    return serial_put_char('0');
+  } else if (value < 0) {
+    serial_put_char('-');
+    value = -value;  // Usar valor absoluto
+  }
 
-	while (value != 0) {
-		buffer[i] = value % 10;
-		value /= 10;
-		i++;
-	}
+  while (value != 0) {
+    buffer[i] = value % 10;
+    value /= 10;
+    i++;
+  }
 
-	if (num_digits < 1 || num_digits > MAX_LONG_DIGITS)
-		num_digits = i;
+  if (num_digits < 1 || num_digits > MAX_LONG_DIGITS) num_digits = i;
 
-	for (int j = num_digits - 1; j > -1; j--)
-		serial_put_char(48 + buffer[j]);
+  for (int j = num_digits - 1; j > -1; j--) serial_put_char(48 + buffer[j]);
 }
 
-void serial_put_int (int value, int num_digits)
-{
-	if (num_digits > MAX_INT_DIGITS)
-		num_digits = MAX_INT_DIGITS;
+void serial_put_int(int value, int num_digits) {
+  if (num_digits > MAX_INT_DIGITS) num_digits = MAX_INT_DIGITS;
 
-	return serial_put_long_int(value, num_digits);
+  return serial_put_long_int(value, num_digits);
 }
 
-void serial_put_double (double value, int int_digits, int frac_digits)
-{
-	long int int_value = (long int) value;
-	double frac_value = value - int_value;
-	int frac_digit = 0;
+void serial_put_double(double value, int int_digits, int frac_digits) {
+  long int int_value = (long int)value;
+  double frac_value = value - int_value;
+  int frac_digit = 0;
 
-	serial_put_long_int(int_value, int_digits);
-	serial_put_char('.');
+  serial_put_long_int(int_value, int_digits);
+  serial_put_char('.');
 
-	if (frac_digits > MAX_DOUBLE_PRECISION)
-		frac_digits = MAX_DOUBLE_PRECISION;
-	else if (frac_digits < 1)
-		frac_digits = 1;
+  if (frac_digits > MAX_DOUBLE_PRECISION)
+    frac_digits = MAX_DOUBLE_PRECISION;
+  else if (frac_digits < 1)
+    frac_digits = 1;
 
-	if (frac_value < 0)
-		frac_value = -frac_value;
+  if (frac_value < 0) frac_value = -frac_value;
 
-	for (int i = 0; i < frac_digits; i++) {
-		frac_value *= 10;
-		frac_digit = (int) frac_value;
-		serial_put_char(48 + (char) frac_value);
-		frac_value -= frac_digit;
-	}
+  for (int i = 0; i < frac_digits; i++) {
+    frac_value *= 10;
+    frac_digit = (int)frac_value;
+    serial_put_char(48 + (char)frac_value);
+    frac_value -= frac_digit;
+  }
 }
 
-char serial_recibido(void)
-{
-	/* COMPLETAR */
-}
-
+char serial_recibido(void) { /* COMPLETAR */ }
--- dario.marin/lab5/cronometro/timer.c
+++ dario.marin/lab5/cronometro/timer.c
@@ -1,6 +1,6 @@
-#include <stdint.h>
-#include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdint.h>
 
 // CONSTANTS
 #define F_OSC 16000000
@@ -19,20 +19,19 @@
 #define PRESCALER_64 ((1 << CS11) | (1 << CS10))
 #define INTERRUPT_ENABLE (1 << OCIE1A)
 
-typedef struct
-{
-    uint8_t tccr1a;
-    uint8_t tccr1b;
-    uint8_t tccr1c;
-    uint8_t reserved;
-    uint8_t tcnt1l;
-    uint8_t tcnt1h;
-    uint8_t icr1l;
-    uint8_t icr1h;
-    uint8_t ocr1al;
-    uint8_t ocr1ah;
-    uint8_t ocr1bl;
-    uint8_t ocr1bh;
+typedef struct {
+  uint8_t tccr1a;
+  uint8_t tccr1b;
+  uint8_t tccr1c;
+  uint8_t reserved;
+  uint8_t tcnt1l;
+  uint8_t tcnt1h;
+  uint8_t icr1l;
+  uint8_t icr1h;
+  uint8_t ocr1al;
+  uint8_t ocr1ah;
+  uint8_t ocr1bl;
+  uint8_t ocr1bh;
 } volatile timer1_t;
 
 timer1_t *timer1 = (timer1_t *)0x80;
@@ -42,30 +41,26 @@ volatile uint8_t hundredths;
 volatile uint8_t seconds;
 volatile uint8_t minutes;
 
-ISR(TIMER1_COMPA_vect)
-{
-    hundredths++;
-    if (hundredths >= 100)
-    {
-        hundredths = 0;
-        seconds++;
-        if (seconds >= 60)
-        {
-            seconds = 0;
-            minutes++;
-        }
+ISR(TIMER1_COMPA_vect) {
+  hundredths++;
+  if (hundredths >= 100) {
+    hundredths = 0;
+    seconds++;
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
     }
+  }
 }
 
-void timer_init()
-{
-    hundredths = 0;
-    seconds = 0;
-    minutes = 0;
+void timer_init() {
+  hundredths = 0;
+  seconds = 0;
+  minutes = 0;
 
-    timer1->tccr1b |= (CTC_MODE | PRESCALER_64);
-    timer1->ocr1ah = (OCR1A_VALUE >> 8) & 0xFF;
-    timer1->ocr1al = OCR1A_VALUE & 0xFF;
-    *timsk1 |= INTERRUPT_ENABLE;
-    sei();
+  timer1->tccr1b |= (CTC_MODE | PRESCALER_64);
+  timer1->ocr1ah = (OCR1A_VALUE >> 8) & 0xFF;
+  timer1->ocr1al = OCR1A_VALUE & 0xFF;
+  *timsk1 |= INTERRUPT_ENABLE;
+  sei();
 }
